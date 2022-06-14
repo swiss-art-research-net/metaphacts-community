7b8ad7f40e64308d7aac67864bf2399ee71e8984
Florian Kräutli <florian.kraeutli@uzh.ch>
2022-06-14 13:47:51 +0200

    make droppable optional

--- metaphacts-platform/web/src/main/components/forms/inputs/AutocompleteInput.tsx
+++ metaphacts-platform/web/src/main/components/forms/inputs/AutocompleteInput.tsx
@@ -54,7 +54,7 @@ import {
 import { ValidationMessages } from './Decorations';
 
 interface SemanticFormAutocompleteInputConfig extends SingleValueInputConfig {
-  droppable: any;
+  droppable?: any;
   template?: string;
   placeholder?: string;
   lookupQuery?: DataQuery;
